0b84a16078e31288fc8bbd441cb2adeed51cccd5
Bryan Elliott <bryan.elliott@bentley.com>
2025-11-07 00:38:50 -0500

    Event more hoisting and sorting

--- index.mjs
+++ index.mjs
@@ -1,19 +1,8 @@
-import { existsSync } from 'node:fs';
-import { readFile } from 'node:fs/promises';
 import { createServer } from 'node:http';
-import { join } from 'node:path';
 
-import { docRoot, getConfig, port } from "./lib/config.mjs";
-import { HttpCode, hydrateRequest, MimeTypes } from "./lib/http.mjs";
+import { getConfig, port } from "./lib/config.mjs";
+import { getStatic, HttpCode, hydrateRequest } from "./lib/http.mjs";
 import { getOauthCallback, getOauthRequest } from './lib/oauth2.mjs';
-
-async function getStatic(request, response) {
-  const staticFile = join(docRoot, ...request.path.split('/'));
-  if (!existsSync(staticFile)) return;
-  const ext = request.path.match(/\.(?<ext>[^\.]+)/)?.groups?.ext;
-  const type = MimeTypes[ext] ?? MimeTypes.txt;
-  return response.writeHead(200, { 'content-type': type }).end(await readFile(staticFile));
-}
 
 createServer(async function (request, response) {
   hydrateRequest(request);
